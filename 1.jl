f056bf2d3bf2c5a27361e643d61443d151611820
Georgeos Hardo <georgeoshardo@protonmail.com>
2021-12-01 18:33:27 +0000

    Update 1.jl

--- 1.jl
+++ 1.jl
@@ -2,3 +2,13 @@ using DelimitedFiles
 file = readdlm("1.txt")
 diffs = diff(file[:,1])
 println(sum(diffs .> 0))
+
+function rolling_diff(file, window)
+    a = []
+    for x ∈ 1:(length(file)-window)
+        push!(a,sum(file[x+1:x+window]) - sum(file[x:x+window-1]))
+    end
+    return a
+end
+println(sum(rolling_diff(file,3) .> 0))
+
